0baf334339af1518cebadca98d208b81b1019855
hamoon jamshidi meydandar <jamshidimeydandar@gmail.com>
2020-08-17 16:39:08 +0200

    Add Firebase Firestore

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@
 target 'Smart Phone' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
+  pod 'Firebase/Firestore'
 
   # Pods for Smart Phone
 
---
+++ Smart Phone/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Smart Phone
+//
+//  Created by Hamoon Jamshidi Meydandar on 2020. 08. 17..
+//
+
+import Foundation
+import SwiftUI
+import firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        
+        
+        return true
+    }
+}
