79a4049eccdc832bad461b7bd41d4d99a9b6ab3c
hamoon jamshidi meydandar <jamshidimeydandar@gmail.com>
2020-08-18 12:57:11 +0200

    Add Pods

--- Smart Phone/AppDelegate.swift
+++ Smart Phone/AppDelegate.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 import SwiftUI
-import firebase
+import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        FirebaseApp.configure()
         
         
         return true
